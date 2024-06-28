65006b734bdae087aeff5a302c499bc330eaf2a8
taketomo3141 <sapphire3141@gmail.com>
2024-06-29 03:46:54 +0900

    userscript用devcontainer追加

---
+++ userscript/rollup.config.ts
@@ -0,0 +1,82 @@
+import { readFileSync } from 'node:fs';
+import { glob } from 'glob';
+import type { RollupOptions } from 'rollup';
+import typescript from '@rollup/plugin-typescript';
+import cleanup from 'rollup-plugin-cleanup';
+import watch from 'rollup-plugin-watch';
+import { stringify } from 'userscript-metadata';
+import type { Metadata } from 'userscript-metadata';
+
+const readMetadata = (path: string): Metadata => JSON.parse(readFileSync(path, 'utf8')) as Metadata;
+const rootDir = process.cwd();
+const entryPaths = glob.sync('src/**/main.ts');
+
+const configs: RollupOptions[] = entryPaths.flatMap(entryPath => {
+    const manifestPath = entryPath.replace(/\/main\.ts$/, '/manifest.json');
+
+    const mainScriptPath = entryPath.replace(/^src\//, 'dist/').replace(/\/(.+)\/main\.ts$/, '/$1.user.js');
+    const devScriptPath = entryPath.replace(/^src\//, 'dist/').replace(/\/(.+)\/main\.ts$/, '/$1.dev.user.js');
+    const mainScriptUrl = `file://${rootDir}/${mainScriptPath}`;
+
+    const devify = (metadata: Metadata): Metadata => {
+        const requires: string[] = [];
+
+        if (typeof metadata.require === 'string') {
+            requires.push(metadata.require);
+        } else if (Array.isArray(metadata.require)) {
+            requires.push(...metadata.require as string[]);
+        }
+
+        requires.push(mainScriptUrl);
+        return {
+            ...metadata,
+            name: `[dev] ${String(metadata.name)}`,
+            require: requires
+        };
+    };
+
+    const mainConfig: RollupOptions = {
+        input: entryPath,
+        output: {
+            file: mainScriptPath,
+            format: 'iife',
+            banner: () => `${stringify(readMetadata(manifestPath))}\n`
+        },
+        plugins: [
+            typescript(),
+            cleanup({
+                extensions: [
+                    'ts'
+                ]
+            }),
+            watch({
+                dir: 'src'
+            })
+        ]
+    };
+
+    const devConfig: RollupOptions = {
+        input: 'src/dev.ts',
+        output: {
+            file: devScriptPath,
+            banner: () => `${stringify(devify(readMetadata(manifestPath)))}\n`
+        },
+        plugins: [
+            typescript(),
+            cleanup({
+                extensions: [
+                    'ts'
+                ]
+            }),
+            watch({
+                dir: 'src'
+            })
+        ]
+    };
+    return [
+        mainConfig,
+        devConfig
+    ];
+});
+
+export default configs;
